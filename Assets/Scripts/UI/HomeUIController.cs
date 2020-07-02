1270075c7f16480e98bc7bb1ec3c01793d63cfcb
John Krukar <jakruka@sandia.gov>
2020-07-02 16:50:02 -0600

    Added Support for Multiple Arena Scenes and Arenas as Asset Bundles
    The folder VEX_Simulator_Files now contains a subdirectory named "Arenas" where asset bundles of arena scenes can be placed. The asset bundles will be loaded as Arena Options.
    
    Scenes that have been added to the build will also appear as Arena Options.
    
    Data from these new Arena Option sources is stored in the JSON so that input and scores are persistently stored.
    
    This will allow students to share arenas that they have designed and challenge each other to program solutions to those arenas

--- Assets/Scripts/UI/HomeUIController.cs
+++ Assets/Scripts/UI/HomeUIController.cs
@@ -12,10 +12,13 @@ public class HomeUIController : MonoBehaviour
     public GameObject ArenaOptionPrefab;
     public GameObject fileOptionPrefab;
     public static string inputFileDirectory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "\\VEX_Simulator_Files";
+    public static string arenasFileDirectory = inputFileDirectory + "\\Arenas";
     private List<string> fileOptions = new List<string>();
+    private List<string> arenaBundles = new List<string>();
     private GameData gameData;
     private Dictionary<string, ArenaOption> arenaOptionMap = new Dictionary<string, ArenaOption>();
     private Dictionary<string, int> inputFileIndexMap = new Dictionary<string, int>();
+    private Dictionary<ArenaOption, bool> isAssetBundleMap = new Dictionary<ArenaOption, bool>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +30,31 @@ public class HomeUIController : MonoBehaviour
             arenaOptionMap.Add(option.name, option);
         }
 
+        GetArenaScenes();
+        GetArenaAssetBundles();     
         GetInputFiles();
 
         foreach (ArenaOption arenaOption in gameData.arenaOptions)
         {
-            BuildArenaOption(arenaOption);
+            bool buildOption = false;
+
+            if(arenaOption.name == "Default Arena" || Application.CanStreamedLevelBeLoaded(arenaOption.name))
+            {
+                buildOption = true;
+                isAssetBundleMap.Add(arenaOption, false);
+                Debug.Log("Added Option: " + arenaOption.name + " - not a bundle");
+            }
+            else if(File.Exists(arenasFileDirectory + "\\" + arenaOption.name))
+            {
+                buildOption = true;
+                isAssetBundleMap.Add(arenaOption, true);
+                Debug.Log("Added Option: " + arenaOption.name + " - bundle");
+            }
+
+            if (buildOption)
+            {
+                BuildArenaOption(arenaOption);
+            }
         }
     }
 
@@ -100,11 +123,89 @@ public class HomeUIController : MonoBehaviour
         SaveGameDataJSON(gameData);
     }
 
+    private void GetArenaScenes()
+    {
+        List<string> scenesInBuild = new List<string>();
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            int lastSlash = scenePath.LastIndexOf("/");
+            scenesInBuild.Add(scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1));
+        }
+
+        List<ArenaOption> arenaOptions = new List<ArenaOption>(gameData.arenaOptions);
+
+        foreach (string scene in scenesInBuild)
+        {
+            if (!arenaOptionMap.ContainsKey(scene))
+            {
+                ArenaOption newOption = new ArenaOption();
+                newOption.name = scene;
+                newOption.inputFile = "";
+
+                arenaOptions.Add(newOption);
+                arenaOptionMap.Add(scene, newOption);
+            }
+        }
+
+        gameData.arenaOptions = arenaOptions.ToArray();
+        SaveGameDataJSON(gameData);
+    }
+
+    private void GetArenaAssetBundles()
+    {
+        if (!Directory.Exists(arenasFileDirectory))
+        {
+            Directory.CreateDirectory(arenasFileDirectory);
+        }
+        else //If the directory exists, look for arenas
+        {
+            foreach (string path in Directory.GetFiles(arenasFileDirectory))
+            {
+                string[] splitPath = path.Split('\\');
+                string fileNameAndExt = splitPath[splitPath.Length - 1];
+                string[] splitFileName = fileNameAndExt.Split('.');
+                string name = splitFileName[0];
+                string extension = "";
+
+                if(splitFileName.Length > 1)
+                {
+                    extension = splitFileName[1];
+                }
+
+                if (extension == "")
+                {
+                    arenaBundles.Add(name);
+                    Debug.Log("Found Arena Bundle: " + name);
+                }
+            }
+
+            List<ArenaOption> arenaOptions = new List<ArenaOption>(gameData.arenaOptions);
+
+            foreach (string arenaBundle in arenaBundles)
+            {
+                if (!arenaOptionMap.ContainsKey(arenaBundle))
+                {
+                    ArenaOption newOption = new ArenaOption();
+                    newOption.name = arenaBundle;
+                    newOption.inputFile = "";
+
+                    arenaOptions.Add(newOption);
+                    arenaOptionMap.Add(arenaBundle, newOption);
+                }
+            }
+
+            gameData.arenaOptions = arenaOptions.ToArray();
+            SaveGameDataJSON(gameData);
+        }
+    }
+
     private void GetInputFiles()
     {
         if (!Directory.Exists(inputFileDirectory))
         {
             Directory.CreateDirectory(inputFileDirectory);
+            Directory.CreateDirectory(arenasFileDirectory);
         }
 
         int dropdownIndex = 0;
@@ -136,7 +237,41 @@ public class HomeUIController : MonoBehaviour
     {
         PlayerPrefs.SetString("currentArena", arena.name);
         PlayerPrefs.SetString("inputFileName", arena.inputFile);
-        SceneManager.LoadScene(arena.name);
+
+        if (isAssetBundleMap[arena])
+        {
+            StartCoroutine(LoadSceneFromBundle(arenasFileDirectory + "\\" + arena.name));
+        }
+        else
+        {
+            SceneManager.LoadScene(arena.name);
+        }        
+    }
+
+    public IEnumerator LoadSceneFromBundle(string path)
+    {
+
+        AssetBundle loadedAssetBundle = AssetBundle.LoadFromFile(path);
+
+        if (loadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            yield return null;
+        }
+        else
+        {
+            if (loadedAssetBundle.isStreamedSceneAssetBundle)
+            {
+                string[] scenePaths = loadedAssetBundle.GetAllScenePaths();
+                string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePaths[0]);
+                AsyncOperation sceneLoad = SceneManager.LoadSceneAsync(sceneName);
+
+                while (!sceneLoad.isDone)
+                {
+                    yield return null;
+                }
+            }
+        }
     }
 
 }
