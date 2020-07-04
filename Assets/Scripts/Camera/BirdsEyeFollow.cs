4c0e27ec5a7fe182d469240077eed067234cfb79
John Krukar <john.a.krukar@gmail.com>
2020-07-04 14:34:56 -0600

    UI updates, Improved Birds Eye Camera
    Adjusted width of file dropdown in home screen
    
    Changed skybox and camera depth culling colors to solid dark blue color
    
    Added a follow script to the bird eye camera and gave it a zoom capability which is controlled by scroll wheel

---
+++ Assets/Scripts/Camera/BirdsEyeFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsEyeFollow : MonoBehaviour
+{
+    private GameObject target;
+    private float zoom = 5.5f;
+    private float minZoom = 0.5f;
+    private float maxZoom =10.0f;
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Bot");
+        camera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        }
+
+        zoom = Mathf.Clamp(zoom - Input.GetAxis("Mouse ScrollWheel") * 5, minZoom, maxZoom);
+        camera.orthographicSize = zoom;
+    }
+}
